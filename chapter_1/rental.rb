73e3a2c2920f37ade80343cac1d1533d4545096b
Y-Fujikawa <fujikawa0@gmail.com>
2020-06-09 09:31:43 +0900

    リファクタリング
    金額計算ルーチンの移動

--- chapter_1/rental.rb
+++ chapter_1/rental.rb
@@ -8,4 +8,22 @@ class Rental
     @movie = movie
     @days_rented = days_rented
   end
+
+  # 各行の金額を計算
+  # chargeメソッドを冪等性（何度実行しても同じ結果になる性質）にできているか確認する
+  # SOLID原則でも同じような原則があるのでそっちで覚えたほうが良い
+  def charge
+    result = 0
+    case movie.price_code
+    when Movie::REGULAR
+      result += 2
+      result += (days_rented - 2) * 1.5 if days_rented > 2
+    when Movie::NEW_RELEASE
+      result += days_rented * 3
+    when Movie::CHILDRENS
+      result += 1.5
+      result += (days_rented - 3) * 1.5 if days_rented > 3
+    end
+    result
+  end
 end
